08470130a7a489994d5365c8fbc9fc9487998658
Misha <bilan.md72@gmail.com>
2023-05-01 12:33:53 +0500

    change

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -1,4 +1,19 @@
 package ru.netology.services;
 public class Main {
-    public int calculate();
+    public int calculate(int income, int expenses, int threshold)  {
+        int counter = 0;
+        int money = 0;
+
+        for (int i = 0; i < 12; i++) {
+            if (money >= threshold) {
+                counter++;
+                money -= expenses;
+                money /= 3;
+            } else {
+                money += (income - expenses);
+            }
+            
+        }
+        return counter;
+    }
 }
--- src/test/java/ru/netology/services/MainTest.java
+++ src/test/java/ru/netology/services/MainTest.java
@@ -1,4 +1,30 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MainTest {
-  
-}
+    @Test
+    public void testCountRestMonthsCase1() {
+        Main main = new Main();
+
+        int actual = main.calculate(10_000, 3_000, 20_000);
+        int expected = 3;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCountRestMonthsCase2() {
+        Main main = new Main();
+
+        int actual = main.calculate(100_000, 60_000, 150_000);
+        int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    }
