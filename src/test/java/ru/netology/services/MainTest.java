9ff50ac8411874fa000a3038ad6ae90034193de0
Misha <bilan.md72@gmail.com>
2023-05-01 13:24:03 +0500

    add csv

--- src/test/java/ru/netology/services/MainTest.java
+++ src/test/java/ru/netology/services/MainTest.java
@@ -2,6 +2,8 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,6 +24,16 @@ class MainTest {
 
         int actual = main.calculate(100_000, 60_000, 150_000);
         int expected = 2;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @ParameterizedTest
+    @CsvFileSource(resources = "test.csv")
+    public void testCountRestMonthsParameterized( int income, int expenses, int threshold, int expected) {
+        Main main = new Main();
+
+        int actual = main.calculate(income, expenses, threshold);
+
 
         Assertions.assertEquals(expected, actual);
     }
